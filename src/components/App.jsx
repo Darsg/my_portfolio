e56c1fd5ebc83ddfd579d7044c7f66a75d0829d7
Darsg <batukdobariya5454@gmail.com>
2025-03-11 18:09:49 +0530

    small change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import "../../src/styles/app.scss"
 import React, {useEffect, useState} from 'react'
-import {useData} from "../src/providers/DataProvider.jsx"
-import Portfolio from "../src/components/Portfolio.jsx"
-import {AnimatedCursor} from "../src/components/feedbacks/AnimatedCursor"
-import ActivitySpinner from "../src/components/feedbacks/ActivitySpinner.jsx"
-import ImageCache from "../src/components/generic/ImageCache.jsx"
-import YoutubeModal from "../src/components/modals/YoutubeModal.jsx"
-import GalleryModal from "../src/components/modals/GalleryModal.jsx"
-import Notifications from "../src/components/feedbacks/Notifications.jsx"
-import ConfirmationWindow from "../src/components/modals/ConfirmationWindow.jsx"
-import {useFeedbacks} from "../src/providers/FeedbacksProvider.jsx"
+import {useData} from "../providers/DataProvider.jsx"
+import Portfolio from "../components/Portfolio.jsx"
+import {AnimatedCursor} from "../components/feedbacks/AnimatedCursor"
+import ActivitySpinner from "../components/feedbacks/ActivitySpinner.jsx"
+import ImageCache from "../components/generic/ImageCache.jsx"
+import YoutubeModal from "../components/modals/YoutubeModal.jsx"
+import GalleryModal from "../components/modals/GalleryModal.jsx"
+import Notifications from "../components/feedbacks/Notifications.jsx"
+import ConfirmationWindow from "../components/modals/ConfirmationWindow.jsx"
+import {useFeedbacks} from "../providers/FeedbacksProvider.jsx"
 
 function App() {
     const {listImagesForCache} = useData()
